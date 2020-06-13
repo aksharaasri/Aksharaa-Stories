02259ad5348cff881f427fabb18b2a4f156c38c6
arunmadhavan-g <arunmadhavan.g@gmail.com>
2020-06-13 23:51:17 +0530

    Adds deploy step, changes the title icon and adds gh-pages support

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,11 +56,12 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `./static/small-girl.png`, // This path is relative to the root of the site.
       },
     },
     // `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
   ],
+  pathPrefix: "Aksharaa-Stories",
 }

b1e356a9419c9066a83b91135ff050f806912393
aksharaasri <65820130+aksharaasri@users.noreply.github.com>
2020-06-29 21:53:08 +0530

    Create index.js

---
+++ content/blog/cheeseTime/index.js
@@ -0,0 +1,24 @@
+One day, Willy mouse planned to throw a cheese party. 
+So he invited his old friends and new friends to the party. 
+
+When the friends arrived, he gave ** Cheese Tea **.
+
+Then he gave ** Chesse with Cheese Chutney **. 
+
+He gave more and more than his friends wanted.
+
+His friends were Angry!
+
+> Willie! How many cheese ? Hunh!!!
+> You gave more than we wanted! 
+
+Said his friends
+
+> Next when you have a party, if you invite us, we will not come.
+
+Willie was sad. His next party was on Monday.
+
+He was sad afterall. 
+
+
+
